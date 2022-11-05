3aaa04d97ffbb9ac2407355d494b7728e495780d
Eva Veli <99196876+evaveli@users.noreply.github.com>
2022-11-05 21:46:59 +0100

    Add files via upload

---
+++ JS Exercises/isShortsWheather.js
@@ -0,0 +1,13 @@
+//I often struggle to know whether I should wear shorts or pants on a given day. (this is a complete lie.  It's really not that hard to decide.) Please help me decide by writing me a function called isShortsWeather. 
+
+//It should accept a single number argument, which we will call temperature(but you can name it whatever you want, of course). 
+
+//If temperature is greater than or equal to 75, return true. Otherwise, return false.  
+
+function isShortsWeather(temperature){
+    if (temperature >= 75) {
+        return true;
+    }
+    return false;
+    
+}
